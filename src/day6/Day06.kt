ed9d1954e311d22df8efc32102da13b2056d3432
Joe Maher <jmaher@skedulo.com>
2022-12-06 18:46:26 +1000

    day 6

---
+++ src/day6/Day06.kt
@@ -0,0 +1,30 @@
+package day6
+
+import Runner
+
+fun main() {
+    Day6Runner().solve()
+}
+
+class Day6Runner : Runner<Int>(
+    day = 6,
+    expectedPartOneTestAnswer = 5,
+    expectedPartTwoTestAnswer = null
+) {
+
+    override fun partOne(input: List<String>): Int {
+        return marker(input, 4)
+    }
+
+    override fun partTwo(input: List<String>): Int {
+        return marker(input, 14)
+    }
+
+    private fun marker(input: List<String>, size: Int) : Int {
+        return input.first()
+            .windowed(size = size)
+            .mapIndexed { index, s -> Pair(index, s.toSet()) }
+            .first { (_, s) -> s.size == size }
+            .first + size
+    }
+}
